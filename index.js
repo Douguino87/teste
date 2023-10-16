038caae0934420757232f42190455df3d2d0fbf7
doug <doug7t@hotmail.com>
2023-10-16 15:52:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Função para redirecionar para o link ao clicar no botão "SIM"
+function sim() {
+  window.location.href = "https://www.youtube.com/watch?v=KXJk8f0xu_c"
+}
+
+function nao() {
+  window.location.href = "https://www.youtube.com/shorts/ffe88y-CclY"
+}
